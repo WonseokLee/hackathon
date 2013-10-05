6d37994a348aef9960cd215e33ca65a4ea5f7490
Wonseok Lee <dlehfql@naver.com>
2013-10-06 02:04:44 +0900

    Fuck Yeah
    gg

---
+++ Hungry/src/com/hackathon/WholeViewActivity.java
@@ -0,0 +1,15 @@
+package com.hackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WholeViewActivity extends Activity{
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+}
