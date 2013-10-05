a4ba48cbd63b1b26f6eb1bd94219e694da75901e
Wonseok Lee <dlehfql@naver.com>
2013-10-05 23:01:42 +0900

    왜 쳐안되고지라 인가....
    후....

--- Hungry/src/com/hackathon/MenuActivity.java
+++ Hungry/src/com/hackathon/MenuActivity.java
@@ -1,5 +1,28 @@
 package com.hackathon;
 
-public class MenuActivity {
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MenuActivity extends Activity implements OnClickListener{
+	SharedPreferences mPrefs;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        
+        mPrefs = getApplicationContext().getSharedPreferences("hackathon", MODE_PRIVATE);
+
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
